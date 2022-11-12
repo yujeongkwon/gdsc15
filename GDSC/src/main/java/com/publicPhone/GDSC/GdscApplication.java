8b5e9609d6ed634a884fbd6efe2e27103e94a54f
yujeongkwon <yoojeongkwon@naver.com>
2022-11-12 18:40:46 +0900

    init

---
+++ GDSC/src/main/java/com/publicPhone/GDSC/GdscApplication.java
@@ -0,0 +1,13 @@
+package com.publicPhone.GDSC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GdscApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GdscApplication.class, args);
+	}
+
+}
---
+++ GDSC/src/test/java/com/publicPhone/GDSC/GdscApplicationTests.java
@@ -0,0 +1,13 @@
+package com.publicPhone.GDSC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GdscApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
